89db023be67e499056b337c626a89486f069bb35
Eddie Kim <eddotman@gmail.com>
2013-09-05 21:28:51 -0400

    added images

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@ $(document).ready( function() {
 
 	var ctx1 = $("#Chart1").get(0).getContext("2d");
 	var ctx2 = $("#Chart2").get(0).getContext("2d");
+	var ctx3 = $("#Chart3").get(0).getContext("2d");
 	var Chart1 = new Chart(ctx1);
 	var Chart2 = new Chart(ctx2);
+	var Chart3 = new Chart(ctx3);
 	
 	var data1 = {
 		labels : ["January","February","March","April","May","June","July"],
@@ -23,7 +25,7 @@ $(document).ready( function() {
 				data : [28,48,40,19,96,27,100]
 			}
 		]
-	}
+	};
 
 	var data2 = [
 		{
@@ -47,8 +49,25 @@ $(document).ready( function() {
 			color : "#4D5360"
 		}
 
-	]
+	];
+
+	var data3 = {
+		labels : ["January","February","March","April","May","June","July"],
+		datasets : [
+			{
+				fillColor : "rgba(220,220,220,0.5)",
+				strokeColor : "rgba(220,220,220,1)",
+				data : [65,59,90,81,56,55,40]
+			},
+			{
+				fillColor : "rgba(151,187,205,0.5)",
+				strokeColor : "rgba(151,187,205,1)",
+				data : [28,48,40,19,96,27,100]
+			}
+		]
+	};
 
 	new Chart(ctx1).Line(data1);
 	new Chart(ctx2).Doughnut(data2);
+	new Chart(ctx3).Bar(data3);
 });
